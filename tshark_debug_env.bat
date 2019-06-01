88cc35a87990321b749d7a317bdd2248409afeb5
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2019-06-01 23:21:17 +0200

    wip

--- tshark_debug_env.bat
+++ tshark_debug_env.bat
@@ -4,5 +4,5 @@ set ZBS=c:\ZeroBraneStudioEduPack-1.80-win32
 set LUA_PATH=.\?.lua;%ZBS%\lualibs/?/?.lua;%ZBS%\lualibs/?.lua
 set LUA_CPATH=%ZBS%\bin/?.dll;%ZBS%\bin/clibs52/?.dll
 set WIRESHARK_NOLUA=1
-set PCAPNG=d:\WiiU\sharkdump\game_capture_emulator_prudpv1.pcapng
+set PCAPNG=d:\WiiU\sharkdump\game_startup4_filter.pcapng
 "c:\Program Files (x86)\Wireshark\tshark.exe" -r "%PCAPNG%" -X lua_script:init_tshark.lua -O prudpv0,prudpv1,nex
